b270749e4af920c126aed9ed2ec4b721dbd15848
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-19 23:13:03 +0900

    feat: add getUserInfoProvider family for fetching user by id

---
+++ lib/features/auth/providers/get_user_info_by_id_provider.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:facebook/core/constants/firebase_collection_names.dart';
+import 'package:facebook/features/auth/models/user.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final getUserInfoProvider =
+    FutureProvider.autoDispose.family<UserModel, String>(
+  (ref, userId) {
+    return FirebaseFirestore.instance
+        .collection(FirebaseCollectionNames.users)
+        .doc(userId)
+        .get()
+        .then(
+      (userData) {
+        return UserModel.fromMap(userData.data()!);
+      },
+    );
+  },
+);
