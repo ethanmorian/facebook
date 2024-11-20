8d5253b56aa3aac82bc75120da0adc8931570cbe
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-20 22:53:00 +0900

    feat: add sign out button and integrate with auth provider

--- lib/core/screens/home_screen.dart
+++ lib/core/screens/home_screen.dart
@@ -1,14 +1,26 @@
+import 'package:facebook/features/auth/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
+  static const routeName = '/home';
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+    WidgetRef ref,
+  ) {
     return Scaffold(
       body: Center(
-        child: Text(
-          'home screen',
+        child: TextButton(
+          onPressed: () {
+            ref.read(authProvider).signOut();
+          },
+          child: Text(
+            'Log out',
+          ),
         ),
       ),
     );
