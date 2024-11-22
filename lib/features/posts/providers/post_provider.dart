d7323115c7f88c73a3a34621efeeeeab948146ee
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-22 23:02:51 +0900

    feat: Create Provider for PostRepository

---
+++ lib/features/posts/providers/post_provider.dart
@@ -0,0 +1,8 @@
+import 'package:facebook/features/posts/repository/post_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final postsProvider = Provider(
+  (ref) {
+    return PostRepository();
+  },
+);
