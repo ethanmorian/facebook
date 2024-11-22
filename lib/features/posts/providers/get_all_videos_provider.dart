c17bc3568c30cbfedd9052324e875eaa2736df5f
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-22 23:03:03 +0900

    feat: Add StreamProvider for fetching all video posts

---
+++ lib/features/posts/providers/get_all_videos_provider.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:facebook/core/constants/firebase_collection_names.dart';
+import 'package:facebook/core/constants/firebase_field_names.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/post.dart';
+
+final getAllVideosProvider = StreamProvider<Iterable<Post>>(
+  (ref) {
+    final controller = StreamController<Iterable<Post>>();
+
+    final sub = FirebaseFirestore.instance
+        .collection(FirebaseCollectionNames.posts)
+        .where(FirebaseFieldNames.postType, isEqualTo: 'video')
+        .orderBy(
+          FirebaseFieldNames.createdAt,
+          descending: true,
+        )
+        .snapshots()
+        .listen(
+      (snapshot) {
+        final posts =
+            snapshot.docs.map((postData) => Post.fromMap(postData.data()));
+        controller.sink.add(posts);
+      },
+    );
+
+    ref.onDispose(
+      () {
+        sub.cancel();
+        controller.close();
+      },
+    );
+
+    return controller.stream;
+  },
+);
