3e0c0ed4226dcd691df289a1a3742b7c78e179c3
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-23 23:08:52 +0900

    feat: Add post interactions with like, comment, and share buttons

---
+++ lib/features/posts/presentation/widgets/post_image_video_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '/features/posts/presentation/widgets/network_video_view.dart';
+
+class PostImageVideoView extends StatelessWidget {
+  final String fileType;
+  final String fileUrl;
+
+  const PostImageVideoView({
+    super.key,
+    required this.fileType,
+    required this.fileUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (fileType == 'image') {
+      return Image.network(fileUrl);
+    } else {
+      return NetworkVideoView(
+        videoUrl: fileUrl,
+      );
+    }
+  }
+}
