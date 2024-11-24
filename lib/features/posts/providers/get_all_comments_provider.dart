b0b8699d8097136aab29e1dd468273f94080e246
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-24 21:03:10 +0900

    feat: add StreamProvider for fetching all comments from Firestore

---
+++ lib/features/posts/providers/get_all_comments_provider.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:facebook/core/constants/firebase_collection_names.dart';
+import 'package:facebook/core/constants/firebase_field_names.dart';
+import 'package:facebook/features/posts/models/comment.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final getAllCommentsProvider = StreamProvider<Iterable<Comment>>(
+  (ref) {
+    final controller = StreamController<Iterable<Comment>>();
+
+    final sub = FirebaseFirestore.instance
+        .collection(FirebaseCollectionNames.comments)
+        .orderBy(
+          FirebaseFieldNames.createdAt,
+          descending: true,
+        )
+        .snapshots()
+        .listen(
+      (snapshot) {
+        final comments = snapshot.docs
+            .map((commentData) => Comment.fromMap(commentData.data()));
+        controller.sink.add(comments);
+      },
+    );
+
+    ref.onDispose(
+      () {
+        sub.cancel();
+        controller.close();
+      },
+    );
+
+    return controller.stream;
+  },
+);
