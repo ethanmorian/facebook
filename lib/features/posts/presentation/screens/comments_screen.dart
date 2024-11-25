7df8fff0a77942cfc512302e4046b57757a5afbf
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-25 16:39:58 +0900

    feat: implement CommentsScreen with comments list and input field

---
+++ lib/features/posts/presentation/screens/comments_screen.dart
@@ -0,0 +1,36 @@
+import 'package:facebook/features/posts/presentation/widgets/comment_text_field.dart';
+import 'package:facebook/features/posts/presentation/widgets/comments_list.dart';
+import 'package:flutter/material.dart';
+
+class CommentsScreen extends StatelessWidget {
+  static const routeName = '/comments';
+  
+  final String postId;
+
+  const CommentsScreen({
+    super.key,
+    required this.postId,
+  });
+
+  // hello world
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Comments'),
+      ),
+      body: Column(
+        children: [
+          // Comments List
+          CommentsList(postId: postId),
+
+          // Comment Text field
+          CommentTextField(
+            postId: postId,
+          ),
+        ],
+      ),
+    );
+  }
+}
