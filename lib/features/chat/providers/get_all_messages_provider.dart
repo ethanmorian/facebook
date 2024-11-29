5c8ef856098e3c322af53275668f808284124d33
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-29 16:24:58 +0900

    feat: add getAllMessagesProvider for real-time message updates

---
+++ lib/features/chat/providers/get_all_messages_provider.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:facebook/core/constants/firebase_collection_names.dart';
+import 'package:facebook/core/constants/firebase_field_names.dart';
+import 'package:facebook/features/chat/models/message.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final getAllMessagesProvider =
+    StreamProvider.autoDispose.family<Iterable<Message>, String>((
+  ref,
+  chatroomId,
+) {
+  final controller = StreamController<Iterable<Message>>();
+
+  final sub = FirebaseFirestore.instance
+      .collection(FirebaseCollectionNames.chatrooms)
+      .doc(chatroomId)
+      .collection(FirebaseCollectionNames.messages)
+      .orderBy(
+        FirebaseFieldNames.timestamp,
+        descending: true,
+      )
+      .snapshots()
+      .listen((snapshot) {
+    final messages = snapshot.docs.map(
+      (messageData) => Message.fromMap(messageData.data()),
+    );
+    controller.sink.add(messages);
+  });
+
+  ref.onDispose(() {
+    controller.close();
+    sub.cancel();
+  });
+
+  return controller.stream;
+});
