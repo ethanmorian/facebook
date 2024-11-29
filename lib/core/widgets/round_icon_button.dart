1688800f9c6a4b67a47432ba611e11ba3f6c203e
ethanmorian <97589894+ethanmorian@users.noreply.github.com>
2024-11-29 16:42:10 +0900

    fix: make RoundIconButton clickable by adding onPressed

--- lib/core/widgets/round_icon_button.dart
+++ lib/core/widgets/round_icon_button.dart
@@ -4,25 +4,28 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class RoundIconButton extends StatelessWidget {
   final IconData icon;
+  final VoidCallback? onPressed;
 
   const RoundIconButton({
     super.key,
     required this.icon,
+    this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 5,
-      ),
-      child: CircleAvatar(
-        backgroundColor: AppColors.greyColor,
-        radius: 20,
-        child: FaIcon(
-          icon,
-          color: AppColors.blackColor,
-          size: 20,
+    return InkWell(
+      onTap: onPressed,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        child: CircleAvatar(
+          backgroundColor: AppColors.greyColor,
+          radius: 20,
+          child: FaIcon(
+            icon,
+            color: AppColors.blackColor,
+            size: 20,
+          ),
         ),
       ),
     );
